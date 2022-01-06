4a9781eea5ec4b4eee3f08691a69e4e150770794
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-06 01:29:50 +0000

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -30,7 +30,10 @@ const Login = () => {
                  <div className='form-content-left '>
                     <h1 className="titulo3 text-8xl text-center text-black mt-56" >Change</h1>
                     <p className='titulo2 text-lg text-center uppercase mt-2 text-black'>Let`s help bring Change to the planet.</p>
-                    <button className="btn text-lg align-center uppercase bold" onClick={changeForm}>{!text ? "Login" : "Register"}</button>
+                    <div className='text-center'>
+                        <p className='mt-11 uppercase texto1'>{!text ? "I want to login instead" : "I want to register instead"}</p>
+                        <button className="btn2 text-lg  mt-5 bold" onClick={changeForm}>{!text ? "Login" : "Register"}</button>
+                    </div>
                  </div>
                  <div className='form-content-right'>
                     {!SignIn ? (<Login_Elements submitForm={submitForm} />) : (<Login_Form submitForm={submitForm} />)}
