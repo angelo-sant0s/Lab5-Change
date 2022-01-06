4bae184ea8cbc116326d42ff1b13a72ffa553c03
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-06 13:36:52 +0000

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,30 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import {FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa";
 
 const Footer = () => {
 
     return (
-        <div className="bg-neutral-800 md:w-full lg:w-9/12 ">
-            <section className="yum flex text-neutral-600 texto2 font-extralight  ml-11 text-2xl">
+        <div className="bg-gradient-to-r from-neutral-900 to-black mt-80 md:w-full lg:w-8/12 grid pt-5 pb-5 grid-cols-8 gap-3 ">
+            <section className="yum text-center col-start-1 col-end-2 text-neutral-400 texto2 font-thin  text-3xl ">
                 <span className="pt-2" to="/">Home</span>
                 <span className="pt-11 pb-11" to="/About">About</span>
                 <span className="pb-2" to="/Login">Login</span>
             </section>
-            <section>
-                <h1 className="text-white titulo3 text-7xl" to='/'>Change</h1>
-                <p className="titulo2 font-extralight uppercase text-white mt-5 text-2xl">Be the Change! Build a bigger world.</p>
+            <section className="col-end-7 col-span-3 s">
+                <h1 className="text-white titulo3 text-7xl  " to='/'>Change</h1>
+                <p className="titulo2 font-extralight uppercase text-white mt-5 text-2xl ">Be the Change.</p>
                 <section className="text-white flex mb-5 mt-11 ">
-                    <span className="text-4xl" href="/" target="_blank" aria-label="Facebook">
+                    <span className="md:text-4xl sm:text-2xl" href="/" target="_blank" aria-label="Facebook">
                         <FaFacebook/>
                     </span>
-                    <span className="mr-9 ml-9 text-4xl " href="/" target="_blank" aria-label="Instagram">
+                    <span className="mr-9 ml-9 md:text-4xl sm:text-2xl " href="/" target="_blank" aria-label="Instagram">
                         <FaInstagram/>
                     </span>
-                    <span className="text-4xl" href="/" target="_blank" aria-label="Twitter">
+                    <span className="md:text-4xl sm:text-2xl" href="/" target="_blank" aria-label="Twitter">
                         <FaTwitter/>
                     </span>
                 </section>
-                <p className="titulo2 font-extralight uppercase text-white text-2xl mt-36">© {new Date().getFullYear()} All rights reserved.</p>
+                <p className="titulo2 font-extralight uppercase text-white text-2xl  mt-36">© {new Date().getFullYear()} All rights reserved.</p>
             </section>
         </div>
     )
