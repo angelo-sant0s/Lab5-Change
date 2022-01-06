fc4b0f3ab1180e316c6c50387f792d172dea5816
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-06 14:59:33 +0000

    C1 About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer.js'
+import C1 from '../components/About_components/C1.js'
 
 const About = () => {
     return (
-        <Footer />
-    )
+        <C1/>
+
+)
 }
 
 export default About;
