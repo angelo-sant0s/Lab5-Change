a8452d4eff113023aa768a2e62567e409abda07c
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-07 17:10:25 +0000

    c3

--- src/components/About_components/C3.js
+++ src/components/About_components/C3.js
@@ -1,7 +1,21 @@
 const C3 = () => {
     return (
         <>
+            <div className="">
+                <div className="grid grid-cols-4 text-right text-white pb-24">
+                    <section className="col-start-3">
+                        <div className="w-1/2 border-b-2"></div>
+                        <h1 className="text-6xl font-bold titulo1 pt-5">Como Vai</h1>
+                        <p className="text-2xl titulo2 font-extralight pt-5">Lorem ipsum dolor sit amet,
+                            consectetuer adipiscing elit,
+                            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                        </p>
+                    </section>
+                    <h1 className="text-9xl font-bold titulo1 text-center">02</h1>
 
+                </div>
+                <img className="w-screen" src="https://i.pinimg.com/564x/7a/c2/41/7ac24106fd985d447c5fc90e60c6a0fd.jpg"/>
+            </div>
         </>
     )
 }
