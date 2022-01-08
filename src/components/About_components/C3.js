b87f0a49457bc8dd8cbca5dcb335f08d68d9ed67
angelo-sant0s <angelosantos@ua.pt>
2022-01-08 00:15:30 +0000

    idk

--- src/components/About_components/C3.js
+++ src/components/About_components/C3.js
@@ -1,20 +1,33 @@
 const C3 = () => {
     return (
         <>
+            <div className="container about-bg py-96 w-screen mr-0">
+                <div className="grid grid-cols-4 text-white">
+                    <h1 className="text-9xl font-bold titulo1 text-center col-span-2 big">01</h1>
+                    <section className="text-left col-start-3 col-span-2">
+                            <div className="w-1/4 -mr-96 border-t-2"></div>
+                             <h1 className="text-6xl font-bold titulo1 pt-5">How it Looks</h1>
+                            <p className="text-2xl titulo2 font-extralight pt-5">Lorem ipsum dolor sit amet,
+                                 consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                             </p>
+                    </section>
+                </div>
+            </div>
             <div className="">
-                <div className="grid grid-cols-4 text-right text-white pb-24">
+                <div className="grid grid-cols-4 text-right text-white py-24">
                     <section className="col-start-3">
-                        <div className="w-1/2 border-b-2"></div>
+                        <div className="w-1/2 -mr-96 border-t-2"></div>
                         <h1 className="text-6xl font-bold titulo1 pt-5">Como Vai</h1>
                         <p className="text-2xl titulo2 font-extralight pt-5">Lorem ipsum dolor sit amet,
                             consectetuer adipiscing elit,
                             sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
                         </p>
                     </section>
-                    <h1 className="text-9xl font-bold titulo1 text-center">02</h1>
+                    <h1 className="text-9xl font-bold titulo1 text-center big">02</h1>
 
                 </div>
-                <img className="w-screen" src="https://i.pinimg.com/564x/7a/c2/41/7ac24106fd985d447c5fc90e60c6a0fd.jpg"/>
+                <img className="w-screen" alt="img1" src="https://i.pinimg.com/564x/7a/c2/41/7ac24106fd985d447c5fc90e60c6a0fd.jpg"/>
             </div>
         </>
     )
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,18 @@
 import React from "react";
 import {FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
 
     return (
         <div className="bg-gradient-to-r from-neutral-800 to-neutral-900 mt-80 md:w-full lg:w-8/12 grid pt-6 pb-5 grid-cols-8 gap-3 ">
             <section className="yum text-center col-start-1 col-end-2 text-neutral-400 texto2 font-thin  text-3xl ">
-                <span className="pt-2" to="/">Home</span>
-                <span className="pt-11 pb-11" to="/About">About</span>
-                <span className="pb-2" to="/Login">Login</span>
+                <Link  className="pt-2" to="/">Home</Link>
+                <Link  className="pt-11 pb-11" to="/About">About</Link>
+                <Link  className="pb-2" to="/Login">Login</Link>
             </section>
             <section className="col-end-7 col-span-3 s">
-                <h1 className="text-white titulo3 text-7xl  " to='/'>Change</h1>
+                <Link className="text-white titulo3 text-7xl" to='/'>Change</Link>
                 <p className="titulo2 font-extralight uppercase text-white mt-5 text-2xl ">Be the Change.</p>
                 <section className="text-white flex mb-5 mt-11 ">
                     <span className="md:text-4xl sm:text-2xl" href="/" target="_blank" aria-label="Facebook">
