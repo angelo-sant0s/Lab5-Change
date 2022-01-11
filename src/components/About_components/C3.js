9291ca368b6230f6cce7d3b2911914e07c07291d
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-11 12:16:45 +0000

    C4

--- src/components/About_components/C3.js
+++ src/components/About_components/C3.js
@@ -1,34 +1,49 @@
 const C3 = () => {
     return (
         <>
-            <div className="container about-bg py-96 w-screen mr-0">
-                <div className="grid grid-cols-4 text-white">
-                    <h1 className="text-9xl font-bold titulo1 text-center col-span-2 big">01</h1>
-                    <section className="text-left col-start-3 col-span-2 pr-24">
-                            <div className="w-1/4 -mr-96 border-t-2"></div>
-                             <h1 className="text-6xl font-bold titulo1 uppercase pt-5">How it Looks</h1>
-                            <p className="text-2xl titulo2 font-extralight text-justify pt-5">Lorem ipsum dolor sit amet,
-                                 consectetuer adipiscing elit,
-                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                             </p>
-                    </section>
-                </div>
-            </div>
-            <div className="">
-                <div className="grid grid-cols-4  text-white gap-11 py-24 pr-16">
-                    <section className="col-start-3">
+            <div className="about-bg">
+                <div className="grid grid-cols-3  text-white gap-11 py-24 ">
+                    <h1 className="text-9xl font-bold titulo1 text-center big">01</h1>
+                    <div className="col-start-2">
                         <div className="w-1/2 -mr-96 border-t-2"></div>
-                        <h1 className="text-6xl font-bold titulo1 uppercase pt-5">Como Vai</h1>
+                        <h1 className="text-5xl font-bold titulo1 uppercase pt-5">Como Vai</h1>
                         <p className="text-2xl titulo2 font-extralight text-justify pt-5">Lorem ipsum dolor sit amet,
                             consectetuer adipiscing elit,
                             sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
                         </p>
-                    </section>
-                    <h1 className="text-9xl font-bold titulo1 text-center big">02</h1>
-
+                    </div>
                 </div>
-                <img className="w-screen" alt="img1" src="https://i.pinimg.com/564x/7a/c2/41/7ac24106fd985d447c5fc90e60c6a0fd.jpg"/>
             </div>
+            <div className="grid grid-cols-3  gap-2  my-24 mx-5  text-center">
+                <section className="text-white titulo2 ">
+                    <p className="font-medium tracking-tight text-3xl">Block</p>
+                    <p className="font-extralight text-3xl">Block and books</p>
+                </section>
+                <section className="text-white titulo2">
+                    <p className="font-medium tracking-tight text-3xl">Block</p>
+                    <p className="font-extralight text-3xl">Block and books</p>
+                </section>
+                <section className="text-white titulo2">
+                    <p className="font-medium tracking-tight text-3xl">Block</p>
+                    <p className="font-extralight text-3xl">Block and books</p>
+                </section>
+            </div>
+
+            <div className="grid grid-cols-3  text-white gap-11 py-24 ">
+                <div className="col-start-2">
+                    <div className="w-1/2 -mr-96 border-t-2"></div>
+                    <h1 className="text-5xl font-bold titulo1 uppercase pt-5">Como Vai</h1>
+                    <p className="text-2xl titulo2 font-extralight text-justify pt-5">Lorem ipsum dolor sit amet,
+                        consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                    </p>
+                </div>
+                <h1 className="text-9xl font-bold titulo1 text-center big">02</h1>
+            </div>
+
+            <img className="w-screen" alt="img1"
+                 src="https://i.pinimg.com/564x/7a/c2/41/7ac24106fd985d447c5fc90e60c6a0fd.jpg"/>
+
         </>
     )
 }
