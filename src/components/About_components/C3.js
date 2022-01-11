82219f7fedb1c553a665cc3a758ee0152de9e7bf
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-11 10:38:57 +0000

    c3

--- src/components/About_components/C3.js
+++ src/components/About_components/C3.js
@@ -4,10 +4,10 @@ const C3 = () => {
             <div className="container about-bg py-96 w-screen mr-0">
                 <div className="grid grid-cols-4 text-white">
                     <h1 className="text-9xl font-bold titulo1 text-center col-span-2 big">01</h1>
-                    <section className="text-left col-start-3 col-span-2">
+                    <section className="text-left col-start-3 col-span-2 pr-24">
                             <div className="w-1/4 -mr-96 border-t-2"></div>
                              <h1 className="text-6xl font-bold titulo1 pt-5">How it Looks</h1>
-                            <p className="text-2xl titulo2 font-extralight pt-5">Lorem ipsum dolor sit amet,
+                            <p className="text-2xl titulo2 font-extralight text-justify pt-5">Lorem ipsum dolor sit amet,
                                  consectetuer adipiscing elit,
                                 sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
                              </p>
@@ -15,11 +15,11 @@ const C3 = () => {
                 </div>
             </div>
             <div className="">
-                <div className="grid grid-cols-4 text-right text-white py-24">
+                <div className="grid grid-cols-4  text-white gap-11 py-24 pr-16">
                     <section className="col-start-3">
                         <div className="w-1/2 -mr-96 border-t-2"></div>
                         <h1 className="text-6xl font-bold titulo1 pt-5">Como Vai</h1>
-                        <p className="text-2xl titulo2 font-extralight pt-5">Lorem ipsum dolor sit amet,
+                        <p className="text-2xl titulo2 font-extralight text-justify pt-5">Lorem ipsum dolor sit amet,
                             consectetuer adipiscing elit,
                             sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
                         </p>
