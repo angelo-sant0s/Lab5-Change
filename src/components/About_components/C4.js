e8aa406881f79b99c714832b44a7e61305ec33d8
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-15 11:34:20 +0000

    Dta done

--- src/components/About_components/C4.js
+++ src/components/About_components/C4.js
@@ -10,18 +10,22 @@ export default class FetchData extends React.Component {
             loading2: true,
             loading3: true,
             loading4: true,
+            loading5: true,
             info: null,
             info2: null,
-            info3:null,
-            info4:null,
+            info3: null,
+            info4: null,
+            info5: null,
             anos: [],
             times: [],
             dates: [],
             anos2: [],
+            dates2: [],
             index: 0,
             index2: 0,
             index3: 0,
-            index4: 0
+            index4: 0,
+            index5: 0,
         }
         /* Primeiro Fetch|API */
         this.handleChange = this.handleChange.bind(this);
@@ -31,6 +35,8 @@ export default class FetchData extends React.Component {
         this.handleChange3 = this.handleChange3.bind(this);
         /* Quarto Fetch|API */
         this.handleChange4 = this.handleChange4.bind(this);
+        /* Quinto Fetch|API */
+        this.handleChange5 = this.handleChange5.bind(this);
     }
 
     /* Primeiro Fetch|API */
@@ -69,6 +75,16 @@ export default class FetchData extends React.Component {
         this.setState({
             index4: this.state.info4.map((e) => {
                 return e.year
+            }).indexOf(event.target.value)
+        })
+    }
+
+    /* Quarto Fetch|API */
+    handleChange5(event) {
+        this.setState({value: event.target.value});
+        this.setState({
+            index5: this.state.info5.map((e) => {
+                return e.date
             }).indexOf(event.target.value)
         })
     }
@@ -116,7 +132,8 @@ export default class FetchData extends React.Component {
                 return response.json();
             })
             .then((data) => {
-                this.setState({info3: data.result, loading3: false});
+                this.setState({info3: data.methane, loading3: false});
+                console.log(this.state.info3)
                 for (let i = 0; i < this.state.info3.length; i++) {
                     this.state.dates.push(this.state.info3[i].date);
                 }
@@ -127,13 +144,15 @@ export default class FetchData extends React.Component {
                     }).indexOf(this.state.value)
                 });
             })
+
+
         /* Quarto Fetch|API */
         fetch("https://global-warming.org/api/co2-api")
             .then(function (response) {
                 return response.json();
             })
             .then((data) => {
-                this.setState({info4: data.result, loading4: false});
+                this.setState({info4: data.co2, loading4: false});
                 for (let i = 0; i < this.state.info4.length; i++) {
                     this.state.anos2.push(this.state.info4[i].year);
                 }
@@ -141,6 +160,23 @@ export default class FetchData extends React.Component {
                 this.setState({
                     index4: this.state.info4.map((e) => {
                         return e.year
+                    }).indexOf(this.state.value)
+                });
+            });
+        /* Quinto Fetch|API */
+        fetch("https://global-warming.org/api/nitrous-oxide-api")
+            .then(function (response) {
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({info5: data.nitrous, loading4: false});
+                for (let i = 0; i < this.state.info5.length; i++) {
+                    this.state.dates2.push(this.state.info5[i].date);
+                }
+                this.setState({value: this.state.info5[this.state.info5.length - 1].date})
+                this.setState({
+                    index5: this.state.info5.map((e) => {
+                        return e.date
                     }).indexOf(this.state.value)
                 });
             });
@@ -157,7 +193,7 @@ export default class FetchData extends React.Component {
                     <div className="flex mb-4 gap-2">
                         <div className="w-1/2 bg-neutral-900 h-72">
                             <div className="border-2 text-black m-4">
-                                /* Primeiro Fetch|API */
+
                                 <div
                                     className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">
                                     01
@@ -181,12 +217,12 @@ export default class FetchData extends React.Component {
                                         </div>
                                     </>) : (<div className="text-white">loading...</div>)}
                                 </div>
-                                /* Fim Primeiro Fetch|API */
+
                             </div>
                         </div>
                         <div className="w-1/2 bg-neutral-900 h-72">
                             <div className="border-2 m-4">
-                                /* Segundo Fetch|API */
+
                                 <div
                                     className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">
                                     02
@@ -210,14 +246,14 @@ export default class FetchData extends React.Component {
                                         </div>
                                     </>) : (<div className="text-white">loading...</div>)}
                                 </div>
-                                /* Fim Segundo Fetch|API */
+
                             </div>
                         </div>
                     </div>
                     <div className="flex gap-2 pb-24">
                         <div className="w-1/5 bg-neutral-900 h-96">
-                            <div className="border-2 pb-80 m-6">
-                                /* Terceiro Fetch|API */
+                            <div className="border-2  m-6">
+
                                 <div
                                     className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">
                                     03
@@ -236,23 +272,23 @@ export default class FetchData extends React.Component {
                                         <div className="text-white titulo2 text-md pt-4">
                                             <p>Average: {this.state.info3[this.state.index3].average}</p>
                                         </div>
-                                        <div className="text-white titulo2 text-md pb-9">
+                                        <div className="text-white titulo2 text-md pb-24">
                                             <p>Trend: {this.state.info3[this.state.index3].trend}</p>
                                         </div>
                                     </>) : (<div className="text-white">loading...</div>)}
                                 </div>
-                                /* Fim Terceiro Fetch|API */</div>
+                            </div>
                         </div>
                         <div className="w-3/5 bg-neutral-900 h-96">
-                            <div className="border-2 pb-80 m-6">
-                                /* Quarta Fetch|API */
+                            <div className="border-2 m-6">
+
                                 <div
                                     className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">
                                     04
                                 </div>
                                 <div className="titulo1 text-white  pl-5  text-2xl">Carbon Dioxide</div>
                                 <div className="pl-5">
-                                    <h1 className="titulo2 uppercase text-white tracking-tight text-md pb-5">See the
+                                    <h1 className="titulo2 uppercase text-white tracking-tight text-md pb-11">See the
                                         year see the change!
                                     </h1>
                                     <select className="rounded px-3 titulo2 hover:bg-black hover:text-white" id="lang"
@@ -264,18 +300,43 @@ export default class FetchData extends React.Component {
                                         <div className="text-white pt-4 titulo2 text-md">
                                             <p>Month: {this.state.info4[this.state.index4].month}</p>
                                         </div>
-                                        <div className="text-white titulo2 text-md pb-9">
+                                        <div className="text-white titulo2 text-md pb-24">
                                             <p>Cycle: {this.state.info4[this.state.index4].cycle}</p>
                                         </div>
                                     </>) : (<div className="text-white">loading...</div>)}
                                 </div>
-                                /* Fim Quarta Fetch|API */</div>
+                            </div>
                         </div>
                         <div className="w-1/5 bg-neutral-900 h-96">
-                            <div className="border-2 pb-80 m-6"></div>
+                            <div className="border-2 m-6">
+
+                                <div
+                                    className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">
+                                    05
+                                </div>
+                                <div className="titulo1 text-white  pl-5  text-2xl">Nitrous Oxide</div>
+                                <div className="pl-5">
+                                    <h1 className="titulo2 uppercase text-white tracking-tight text-md pb-5">See the
+                                        date see the change!
+                                    </h1>
+                                    <select className="rounded px-3 titulo2 hover:bg-black hover:text-white" id="lang"
+                                            onChange={this.handleChange5} value={this.state.value}>
+                                        {this.state.dates2.map((date2) =>
+                                            <option key={date2} value={date2}>{date2}</option>)}
+                                    </select>
+                                    {this.state.info5 && this.state.index5 ? (<>
+                                        <div className="text-white titulo2 text-md pt-4">
+                                            <p>Average: {this.state.info5[this.state.index5].average}</p>
+                                        </div>
+                                        <div className="text-white titulo2 text-md pb-24">
+                                            <p>Trend: {this.state.info5[this.state.index5].trend}</p>
+                                        </div>
+                                    </>) : (<div className="text-white">loading...</div>)}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <Link className="titulo2 btn text-2xl" to="/Profile">Descobre o teu</Link>
+                    <Link className="btn px-8 py-3 titulo2 text-2xl" to="/Profile">Find Yours</Link>
                 </div>
             </>
         )
