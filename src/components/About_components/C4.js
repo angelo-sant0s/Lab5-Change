d0b8f9d152ff5163f4eafde11d1ed5176030e5f3
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-15 00:11:47 +0000

    ta td mal

--- src/components/About_components/C4.js
+++ src/components/About_components/C4.js
@@ -6,20 +6,34 @@ export default class FetchData extends React.Component {
         super(props);
         this.state = {
             value: 'Select Year',
-            value2: 0,
             loading: true,
             loading2: true,
+            loading3: true,
+            loading4: true,
             info: null,
             info2: null,
+            info3:null,
+            info4:null,
             anos: [],
             times: [],
+            dates: [],
+            anos2: [],
             index: 0,
-            index2: 0
+            index2: 0,
+            index3: 0,
+            index4: 0
         }
+        /* Primeiro Fetch|API */
         this.handleChange = this.handleChange.bind(this);
+        /* Segundo Fetch|API */
         this.handleChange2 = this.handleChange2.bind(this);
+        /* Terceiro Fetch|API */
+        this.handleChange3 = this.handleChange3.bind(this);
+        /* Quarto Fetch|API */
+        this.handleChange4 = this.handleChange4.bind(this);
     }
 
+    /* Primeiro Fetch|API */
     handleChange(event) {
         this.setState({value: event.target.value});
         this.setState({
@@ -28,8 +42,10 @@ export default class FetchData extends React.Component {
             }).indexOf(event.target.value)
         })
     }
-    handleChange2(event){
-        this.setState({value2: event.target.value});
+
+    /* Segundo Fetch|API */
+    handleChange2(event) {
+        this.setState({value: event.target.value});
         this.setState({
             index2: this.state.info2.map((e) => {
                 return e.time
@@ -37,8 +53,28 @@ export default class FetchData extends React.Component {
         })
     }
 
+    /* Terceiro Fetch|API */
+    handleChange3(event) {
+        this.setState({value: event.target.value});
+        this.setState({
+            index3: this.state.info3.map((e) => {
+                return e.date
+            }).indexOf(event.target.value)
+        })
+    }
+
+    /* Quarto Fetch|API */
+    handleChange4(event) {
+        this.setState({value: event.target.value});
+        this.setState({
+            index4: this.state.info4.map((e) => {
+                return e.year
+            }).indexOf(event.target.value)
+        })
+    }
+
     async componentDidMount() {
-        /* Primeiro Fetch */
+        /* Primeiro Fetch|API */
         fetch("https://global-warming.org/api/arctic-api")
             .then(function (response) {
                 return response.json();
@@ -57,24 +93,58 @@ export default class FetchData extends React.Component {
                 });
             });
 
-        /* Segundo Fetch */
+        /* Segundo Fetch|API */
         fetch("https://global-warming.org/api/temperature-api")
             .then(function (response) {
                 return response.json();
             })
             .then((data) => {
                 this.setState({info2: data.result, loading2: false});
-                for (let i = 0; i < this.state.info2.length ; i++) {
-                     this.state.times.push(this.state.info2[i].time);
+                for (let i = 0; i < this.state.info2.length; i++) {
+                    this.state.times.push(this.state.info2[i].time);
                 }
-                 this.setState({value2: this.state.info2[this.state.info2.length - 1].time})
-                 this.setState({
+                this.setState({value: this.state.info2[this.state.info2.length - 1].time})
+                this.setState({
                     index2: this.state.info2.map((e) => {
                         return e.time
-                    }).indexOf(this.state.value2)
+                    }).indexOf(this.state.value)
                 });
             })
-           
+        /* Terceiro Fetch|API */
+        fetch("https://global-warming.org/api/methane-api")
+            .then(function (response) {
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({info3: data.result, loading3: false});
+                for (let i = 0; i < this.state.info3.length; i++) {
+                    this.state.dates.push(this.state.info3[i].date);
+                }
+                this.setState({value: this.state.info3[this.state.info3.length - 1].date})
+                this.setState({
+                    index3: this.state.info3.map((e) => {
+                        return e.date
+                    }).indexOf(this.state.value)
+                });
+            })
+        /* Quarto Fetch|API */
+        fetch("https://global-warming.org/api/co2-api")
+            .then(function (response) {
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({info4: data.result, loading4: false});
+                for (let i = 0; i < this.state.info4.length; i++) {
+                    this.state.anos2.push(this.state.info4[i].year);
+                }
+                this.setState({value: this.state.info4[this.state.info4.length - 1].year})
+                this.setState({
+                    index4: this.state.info4.map((e) => {
+                        return e.year
+                    }).indexOf(this.state.value)
+                });
+            });
+
     }
 
     render() {
@@ -87,6 +157,7 @@ export default class FetchData extends React.Component {
                     <div className="flex mb-4 gap-2">
                         <div className="w-1/2 bg-neutral-900 h-72">
                             <div className="border-2 text-black m-4">
+                                /* Primeiro Fetch|API */
                                 <div
                                     className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">
                                     01
@@ -108,42 +179,97 @@ export default class FetchData extends React.Component {
                                         <div className="text-white titulo2 text-md pb-9">
                                             <p>Area: {this.state.info[this.state.index].area}</p>
                                         </div>
-
-                                    </>) : (<div>loading...</div>)}
+                                    </>) : (<div className="text-white">loading...</div>)}
                                 </div>
+                                /* Fim Primeiro Fetch|API */
                             </div>
                         </div>
                         <div className="w-1/2 bg-neutral-900 h-72">
                             <div className="border-2 m-4">
-                                <div className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">02</div>
+                                /* Segundo Fetch|API */
+                                <div
+                                    className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">
+                                    02
+                                </div>
                                 <div className="titulo1 text-white  pl-5  text-2xl">Global Temperature Anomalies</div>
                                 <div className="pl-5">
                                     <h1 className="titulo2 uppercase text-white tracking-tight text-md pb-5">See the
-                                        year see the change!
+                                        time see the change!
                                     </h1>
                                     <select className="rounded px-3 titulo2 hover:bg-black hover:text-white" id="lang"
-                                            onChange={this.handleChange2} value={this.state.value2}>
+                                            onChange={this.handleChange2} value={this.state.value}>
                                         {this.state.times.map((time) =>
                                             <option key={time} value={time}>{time}</option>)}
                                     </select>
                                     {this.state.info2 && this.state.index2 ? (<>
-                                        <div className="text-white titulo2 text-md">
+                                        <div className="text-white titulo2 text-md pt-4">
                                             <p>Station: {this.state.info2[this.state.index2].station}</p>
                                         </div>
-                                        <div className="text-white titulo2 text-md pb-3">
+                                        <div className="text-white titulo2 text-md pb-9">
                                             <p>Land: {this.state.info2[this.state.index2].land}</p>
                                         </div>
-                                    </>) : (<div>loading...</div>)}
+                                    </>) : (<div className="text-white">loading...</div>)}
                                 </div>
+                                /* Fim Segundo Fetch|API */
                             </div>
                         </div>
                     </div>
                     <div className="flex gap-2 pb-24">
                         <div className="w-1/5 bg-neutral-900 h-96">
-                            <div className="border-2 pb-80 m-6"></div>
+                            <div className="border-2 pb-80 m-6">
+                                /* Terceiro Fetch|API */
+                                <div
+                                    className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">
+                                    03
+                                </div>
+                                <div className="titulo1 text-white  pl-5 text-2xl">Methane</div>
+                                <div className="pl-5">
+                                    <h1 className="titulo2 uppercase text-white tracking-tight text-md pb-5">See the
+                                        date see the change!
+                                    </h1>
+                                    <select className="rounded px-3 titulo2 hover:bg-black hover:text-white" id="lang"
+                                            onChange={this.handleChange3} value={this.state.value}>
+                                        {this.state.dates.map((date) =>
+                                            <option key={date} value={date}>{date}</option>)}
+                                    </select>
+                                    {this.state.info3 && this.state.index3 ? (<>
+                                        <div className="text-white titulo2 text-md pt-4">
+                                            <p>Average: {this.state.info3[this.state.index3].average}</p>
+                                        </div>
+                                        <div className="text-white titulo2 text-md pb-9">
+                                            <p>Trend: {this.state.info3[this.state.index3].trend}</p>
+                                        </div>
+                                    </>) : (<div className="text-white">loading...</div>)}
+                                </div>
+                                /* Fim Terceiro Fetch|API */</div>
                         </div>
                         <div className="w-3/5 bg-neutral-900 h-96">
-                            <div className="border-2 pb-80 m-6"></div>
+                            <div className="border-2 pb-80 m-6">
+                                /* Quarta Fetch|API */
+                                <div
+                                    className="titulo2 font-semibold text-white text-right tracking-tight pr-5 py-3 text-lg">
+                                    04
+                                </div>
+                                <div className="titulo1 text-white  pl-5  text-2xl">Carbon Dioxide</div>
+                                <div className="pl-5">
+                                    <h1 className="titulo2 uppercase text-white tracking-tight text-md pb-5">See the
+                                        year see the change!
+                                    </h1>
+                                    <select className="rounded px-3 titulo2 hover:bg-black hover:text-white" id="lang"
+                                            onChange={this.handleChange4} value={this.state.value}>
+                                        {this.state.anos2.map((ano2) =>
+                                            <option key={ano2} value={ano2}>{ano2}</option>)}
+                                    </select>
+                                    {this.state.info4 && this.state.index4 ? (<>
+                                        <div className="text-white pt-4 titulo2 text-md">
+                                            <p>Month: {this.state.info4[this.state.index4].month}</p>
+                                        </div>
+                                        <div className="text-white titulo2 text-md pb-9">
+                                            <p>Cycle: {this.state.info4[this.state.index4].cycle}</p>
+                                        </div>
+                                    </>) : (<div className="text-white">loading...</div>)}
+                                </div>
+                                /* Fim Quarta Fetch|API */</div>
                         </div>
                         <div className="w-1/5 bg-neutral-900 h-96">
                             <div className="border-2 pb-80 m-6"></div>
