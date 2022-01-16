afb9994c972024475d1b7f04d8adfb665de62775
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-16 20:11:42 +0000

    about

--- src/components/About_components/C3.js
+++ src/components/About_components/C3.js
@@ -46,20 +46,41 @@ const C3 = () => {
 
             <img className="w-screen" alt="img1"
                  src="https://images.unsplash.com/photo-1615092296061-e2ccfeb2f3d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
-
-            <div className="grid grid-cols-3  text-white gap-11 pt-32 ">
-                <h1 className="text-9xl font-bold titulo1 text-center big">03</h1>
-                <div className="col-start-2">
+            <div className="grid grid-cols-2  text-white gap-11 pt-48 pb-11 ">
+                <div className="pl-40">
                     <div className="w-1/2 -mr-96 border-t-2"></div>
-                    <h1 className="text-5xl font-bold titulo1 uppercase pt-5">Methane</h1>
-                    <p className="text-2xl titulo2 font-extralight text-justify pt-5">Lorem ipsum dolor sit amet,
+                    <h1 className="text-5xl text-left font-bold titulo1 uppercase pt-5">Methane</h1>
+                    <p className="text-2xl titulo2 font-extralight text-justify pt-5 pr-24">Lorem ipsum dolor sit amet,
                         consectetuer adipiscing elit,
                         sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
                     </p>
                 </div>
+                <h1 className="text-9xl pr-40 text-right font-bold titulo1 text-center big">03</h1>
+            </div>
+            <div className="about3-bg py-2 px-40">
+                <h1 className="font-medium text-white tracking-tight text-3xl pb-11">Carbon Dioxide</h1>
+                <div className="grid grid-cols-3 gap-24 text-justify">
+                    <p className="text-2xl titulo2 text-white font-extralight pb-80">Lorem ipsum dolor sit amet.
+                    </p>
+                    <p className="text-2xl titulo2 text-white font-extralight">Lorem ipsum dolor sit amet.
+                    </p>
+                    <p className="text-2xl titulo2 text-white font-extralight">Lorem ipsum dolor sit amet.
+                    </p>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 text-white gap-11 pt-48 pb-11 ">
+                <div className="pl-20">
+                    <div className="w-1/2 -mr-96 border-t-2"></div>
+                    <h1 className="text-5xl text-letf font-bold titulo1 uppercase pt-5">Carbon Dioxide</h1>
+                    <p className="text-2xl titulo2 font-extralight text-justify pt-5 pr-24">Lorem ipsum dolor sit amet,
+                        consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                    </p>
+                </div>
+                <h1 className="text-9xl pr-20 font-bold titulo1 text-right big">04</h1>
             </div>
 
-            <div className="grid grid-cols-2  pt-48">
+            <div className="grid grid-cols-2 pt-24  pb-48">
                 <div className="cols-span-2">
                     <img className="" alt="img1"
                          src="https://i.imgur.com/mlkT4Hp.png"/>
@@ -78,49 +99,18 @@ const C3 = () => {
                     </p>
                 </div>
             </div>
-            <div className="grid grid-cols-2  text-white gap-11 pt-48 pb-11 ">
-                <div className="pl-40">
-                    <div className="w-1/2 -mr-96 border-t-2"></div>
-                    <h1 className="text-5xl font-bold titulo1 uppercase pt-5">Carbon Dioxide</h1>
-                    <p className="text-2xl titulo2 font-extralight text-justify pt-5 pr-24">Lorem ipsum dolor sit amet,
-                        consectetuer adipiscing elit,
-                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                    </p>
-                </div>
-                <h1 className="text-9xl pl-16 font-bold titulo1 text-center big">04</h1>
-            </div>
-            <div className="about3-bg py-2 px-40">
-                <h1 className="font-medium text-white tracking-tight text-3xl pb-11">Carbon Dioxide</h1>
-                <div className="grid grid-cols-3 gap-24 text-justify">
-                    <p className="text-2xl titulo2 text-white font-extralight pb-80">Lorem ipsum dolor sit amet.
-                    </p>
-                    <p className="text-2xl titulo2 text-white font-extralight">Lorem ipsum dolor sit amet.
-                    </p>
-                    <p className="text-2xl titulo2 text-white font-extralight">Lorem ipsum dolor sit amet.
-                    </p>
-                </div>
-            </div>
+
             <div className="about4-bg">
-                <div className="grid grid-cols-4  gap-11 text-white  py-24 ">
-                    <h1 className="pl-20 text-9xl font-bold titulo1 text-center big pt-11">05</h1>
-                    <div className="col-span-2 pl-36 px-20 pt-11">
+                <div className="grid grid-cols-3  text-white gap-11 py-24 ">
+                    <h1 className="text-9xl font-bold titulo1 text-center big">05</h1>
+                    <div className="col-start-2">
                         <div className="w-1/2 -mr-96 border-t-2"></div>
                         <h1 className="text-5xl font-bold titulo1 uppercase pt-5">Nitrous Oxide</h1>
-                        <p className="text-2xl titulo2 font-extralight text-justify pt-5 pr-11">Lorem ipsum dolor sit
-                            amet,
+                        <p className="text-2xl titulo2 font-extralight text-justify pt-5">Lorem ipsum dolor sit amet,
                             consectetuer adipiscing elit,
                             sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                        </p>
-                        <p className="text-2xl titulo2 font-extralight text-justify pt-5 pr-11">Lorem ipsum dolor sit
-                            amet,
-                            consectetuer adipiscing elit,
-                            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                        </p>
-                        <p className="text-2xl titulo2 font-extralight text-justify pt-5 pr-11">Lorem ipsum dolor sit
-                            amet.
                         </p>
                     </div>
-
                 </div>
             </div>
             <div className="grid grid-cols-3 gap-5 text-center  text-white  pt-48 pb-28 px-32 ">
