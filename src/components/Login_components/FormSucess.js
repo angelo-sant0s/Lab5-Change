207cd7c21423b99f8d0b9d344a8f7e791b32df76
Martiinhaa <raquel.azevedo11@ua.pt>
2022-01-27 00:26:45 +0000

    Quality life changes

--- src/components/Login_components/FormSucess.js
+++ src/components/Login_components/FormSucess.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Form.css';
+import "./FormBack/Streamgraph"
 
 const FormSucess = () => {
     return (
         <div className='form-content-right'>
-            <h1 className='form-success'>You are logged in!</h1>
+            <h1 className='form-success font-extralight uppercase py-11'>You are logged in!</h1>
+            <Example />
         </div>
     );
 };
