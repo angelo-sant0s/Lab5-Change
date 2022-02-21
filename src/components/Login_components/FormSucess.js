4171da8a2d3afb6484e19957370499c05d297b4e
Martiinhaa <raquel.azevedo11@ua.pt>
2022-02-21 01:18:07 +0000

    Quality of life Changes

--- src/components/Login_components/FormSucess.js
+++ src/components/Login_components/FormSucess.js
@@ -4,7 +4,7 @@ import './Form.css';
 const FormSucess = () => {
     return (
         <div className='form-content-right'>
-            <h1 className='form-success font-extralight uppercase py-11'>You are logged in!</h1>
+            <h1 className='form-success titulo1 text-white text-4xl text-center pt-64'>You are logged in!</h1>
         </div>
     );
 };
